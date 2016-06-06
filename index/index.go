eeb786e6e66833f59e65e12d410725b8b5c93933
Alex Yatskov <alex@foosoft.net>
2016-06-05 18:18:55 -0700

    Improvements to index plugin

--- index/index.go
+++ index/index.go
@@ -31,20 +31,20 @@ import (
 )
 
 type index struct {
-	indexFile string
-	key       string
-	meta      map[string]interface{}
+	file string
+	key  string
+	meta map[string]interface{}
 
 	dirs    map[string]*dirSummary
 	dirsMtx sync.Mutex
 }
 
-func New(indexFile, key string, meta map[string]interface{}) goldsmith.Plugin {
+func New(file, key string, meta map[string]interface{}) goldsmith.Plugin {
 	return &index{
-		indexFile: indexFile,
-		key:       key,
-		meta:      meta,
-		dirs:      make(map[string]*dirSummary),
+		file: file,
+		key:  key,
+		meta: meta,
+		dirs: make(map[string]*dirSummary),
 	}
 }
 
@@ -59,38 +59,46 @@ func (i *index) Process(ctx goldsmith.Context, f goldsmith.File) error {
 
 	for {
 		dir := path.Dir(curr)
-		ds, ok := i.dirs[dir]
-		if !ok {
-			ds = new(dirSummary)
-			i.dirs[dir] = ds
-		}
-
 		base := path.Base(curr)
-		if base == i.indexFile {
-			ds.hasIndex = true
+
+		summary, ok := i.dirs[dir]
+		if !ok {
+			summary = &dirSummary{Name: path.Base(dir), Path: dir}
+			i.dirs[dir] = summary
 		}
 
-		ds.items = append(ds.items, DirItem{Name: base, Dir: !leaf})
+		if base == i.file {
+			summary.hasIndex = true
+		}
 
-		curr = dir
-		leaf = false
+		record := DirRecord{
+			Name:  base,
+			Path:  curr,
+			IsDir: !leaf,
+		}
+
+		summary.Records = append(summary.Records, record)
 
 		if dir == "." {
 			break
 		}
+
+		curr = dir
+		leaf = false
+
 	}
 
 	return nil
 }
 
 func (i *index) Finalize(ctx goldsmith.Context) error {
-	for dn, ds := range i.dirs {
-		if ds.hasIndex {
+	for name, summary := range i.dirs {
+		if summary.hasIndex {
 			continue
 		}
 
-		f := goldsmith.NewFileFromData(path.Join(dn, i.indexFile), make([]byte, 0))
-		f.SetValue(i.key, ds.items)
+		f := goldsmith.NewFileFromData(path.Join(name, i.file), make([]byte, 0))
+		f.SetValue(i.key, summary)
 		for name, value := range i.meta {
 			f.SetValue(name, value)
 		}
@@ -102,34 +110,37 @@ func (i *index) Finalize(ctx goldsmith.Context) error {
 }
 
 type dirSummary struct {
+	Name     string
+	Path     string
+	Records  DirRecords
 	hasIndex bool
-	items    DirItems
 }
 
-type DirItem struct {
-	Name string
-	Dir  bool
+type DirRecord struct {
+	Name  string
+	Path  string
+	IsDir bool
 }
 
-type DirItems []DirItem
+type DirRecords []DirRecord
 
-func (d DirItems) Len() int {
+func (d DirRecords) Len() int {
 	return len(d)
 }
 
-func (d DirItems) Less(i, j int) bool {
+func (d DirRecords) Less(i, j int) bool {
 	d1, d2 := d[i], d[j]
 
-	if d1.Dir && !d2.Dir {
+	if d1.IsDir && !d2.IsDir {
 		return true
 	}
-	if !d1.Dir && d2.Dir {
+	if !d1.IsDir && d2.IsDir {
 		return false
 	}
 
 	return strings.Compare(d1.Name, d2.Name) == -1
 }
 
-func (d DirItems) Swap(i, j int) {
+func (d DirRecords) Swap(i, j int) {
 	d[i], d[j] = d[j], d[i]
 }
