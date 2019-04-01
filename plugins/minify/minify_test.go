d347b23f96a47716919c31e22fd22b8180a28c43
Alex Yatskov <alex@foosoft.net>
2019-04-01 13:11:56 -0700

    add unit tests for minify

---
+++ plugins/minify/minify_test.go
@@ -0,0 +1,17 @@
+package minify
+
+import (
+	"testing"
+
+	"github.com/FooSoft/goldsmith"
+	"github.com/FooSoft/goldsmith-components/harness"
+)
+
+func Test(t *testing.T) {
+	harness.Validate(
+		t,
+		func(gs *goldsmith.Goldsmith) {
+			gs.Chain(New())
+		},
+	)
+}
