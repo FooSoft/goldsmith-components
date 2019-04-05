a7e2777a298265553c8b1d7bea90f2f2d393b6e5
Alex Yatskov <alex@foosoft.net>
2019-04-05 12:17:04 -0700

    final cleanup pass on thumbnail plugin

--- plugins/thumbnail/thumbnail.go
+++ plugins/thumbnail/thumbnail.go
@@ -1,3 +1,5 @@
+// Package thumbnail automatically generates thumbnails for a variety of common
+// image formats and saves them to a user-configurable path.
 package thumbnail
 
 import (
@@ -15,28 +17,36 @@ import (
 	"github.com/nfnt/resize"
 )
 
-type Namer func(string, uint) (string, bool)
+// Namer callback function builds thumbnail file paths based on the original file path.
+// An empty path can be returned if a thumbnail should not be generated for the current file.
+type Namer func(string, uint) string
 
+// Thumbnail chainable context.
 type Thumbnail struct {
 	size  uint
 	namer Namer
 }
 
+// New creates a new instance of the Thumbnail plugin.
 func New() *Thumbnail {
-	namer := func(path string, dims uint) (string, bool) {
+	namer := func(path string, dims uint) string {
 		ext := filepath.Ext(path)
 		body := strings.TrimSuffix(path, ext)
-		return fmt.Sprintf("%s-thumb.png", body), true
+		return fmt.Sprintf("%s-thumb.png", body)
 	}
 
 	return &Thumbnail{128, namer}
 }
 
+// Size sets the desired maximum pixel size of generated thumbnails (default: 128).
 func (plugin *Thumbnail) Size(dims uint) *Thumbnail {
 	plugin.size = dims
 	return plugin
 }
 
+// Namer sets the callback used to build paths for thumbnail files.
+// Default naming appends "-thumb" to the path and changes extension to PNG,
+// for example "file.jpg" becomes "file-thumb.png".
 func (plugin *Thumbnail) Namer(namer Namer) *Thumbnail {
 	plugin.namer = namer
 	return plugin
@@ -53,8 +63,8 @@ func (*Thumbnail) Initialize(context *goldsmith.Context) (goldsmith.Filter, erro
 func (plugin *Thumbnail) Process(context *goldsmith.Context, inputFile *goldsmith.File) error {
 	defer context.DispatchFile(inputFile)
 
-	thumbPath, create := plugin.namer(inputFile.Path(), plugin.size)
-	if !create {
+	thumbPath := plugin.namer(inputFile.Path(), plugin.size)
+	if len(thumbPath) == 0 {
 		return nil
 	}
 
