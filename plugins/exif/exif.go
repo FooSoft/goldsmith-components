9ecc6502ac336e7c1e603a8729a39149199491e4
Alex Yatskov <alex@foosoft.net>
2019-07-06 14:04:57 -0700

    add timestamps

--- plugins/exif/exif.go
+++ plugins/exif/exif.go
@@ -39,7 +39,8 @@ type geoData struct {
 }
 
 type exifData struct {
-	Geo *geoData
+	Geo  *geoData
+	Time *time.Time
 }
 
 // New creates new instance of the Exif plugin.
@@ -61,64 +62,90 @@ func (plugin *Exif) Initialize(context *goldsmith.Context) (goldsmith.Filter, er
 }
 
 func (plugin *Exif) Process(context *goldsmith.Context, inputFile *goldsmith.File) error {
-	var exifData exifData
-	if geoData, err := plugin.extractGeo(inputFile); err == nil {
-		exifData.Geo = geoData
+	rawFile, err := ioutil.ReadAll(inputFile)
+	if err != nil {
+		return err
 	}
 
-	inputFile.Meta[plugin.dataKey] = exifData
+	inputFile.Meta[plugin.dataKey] = plugin.extractExif(rawFile)
 	context.DispatchFile(inputFile)
+
 	return nil
 }
 
-// Based on https://godoc.org/github.com/dsoprea/go-exif#example-Ifd-GpsInfo
-func (plugin *Exif) extractGeo(file *goldsmith.File) (*geoData, error) {
-	rawFile, err := ioutil.ReadAll(file)
-	if err != nil {
-		return nil, err
-	}
-
+func (plugin *Exif) extractExif(rawFile []byte) (exifData exifData) {
 	rawExif, err := exif.SearchAndExtractExif(rawFile)
 	if err != nil {
-		return nil, err
+		return
 	}
 
 	im := exif.NewIfdMapping()
 	if err := exif.LoadStandardIfds(im); err != nil {
-		return nil, err
+		return
 	}
 
-	ti := exif.NewTagIndex()
-	_, index, err := exif.Collect(im, ti, rawExif)
+	_, index, err := exif.Collect(im, exif.NewTagIndex(), rawExif)
 	if err != nil {
-		return nil, err
+		return
 	}
 
-	ifd, err := index.RootIfd.ChildWithIfdPath(exif.IfdPathStandardGps)
+	exifData.Time = extractTimestamp(index.RootIfd)
+	if exifData.Geo = extractGeo(index.RootIfd); exifData.Geo != nil {
+		if plugin.lookuper != nil {
+			exifData.Geo.Lookup, _ = plugin.lookuper.Lookup(exifData.Geo.Latitude, exifData.Geo.Longitude)
+		}
+	}
+
+	return
+}
+
+func extractGeo(rootIfd *exif.Ifd) *geoData {
+	ifd, err := rootIfd.ChildWithIfdPath(exif.IfdPathStandardGps)
 	if err != nil {
-		return nil, err
+		return nil
 	}
 
 	gi, err := ifd.GpsInfo()
 	if err != nil {
-		return nil, err
+		return nil
 	}
 
-	latitude := gi.Latitude.Decimal()
-	longitude := gi.Longitude.Decimal()
+	return &geoData{gi.Latitude.Decimal(), gi.Longitude.Decimal(), gi.Altitude, gi.Timestamp, nil}
+}
 
-	var lookupData *LookupData
-	if plugin.lookuper != nil {
-		lookupData, _ = plugin.lookuper.Lookup(latitude, longitude)
+func extractTimestamp(rootIfd *exif.Ifd) *time.Time {
+	if exifIfd, err := rootIfd.ChildWithIfdPath(exif.IfdPathStandardExif); err == nil {
+		if timestamp := extractTimestampByName(exifIfd, "DateTimeOriginal"); timestamp != nil {
+			return timestamp
+		}
+		if timestamp := extractTimestampByName(exifIfd, "DateTimeDigitized"); timestamp != nil {
+			return timestamp
+		}
 	}
 
-	data := &geoData{
-		latitude,
-		longitude,
-		gi.Altitude,
-		gi.Timestamp,
-		lookupData,
+	return extractTimestampByName(rootIfd, "DateTime")
+}
+
+func extractTimestampByName(ifd *exif.Ifd, tagName string) *time.Time {
+	results, err := ifd.FindTagWithName(tagName)
+	if err != nil || len(results) == 0 {
+		return nil
 	}
 
-	return data, nil
+	valueRaw, err := ifd.TagValue(results[0])
+	if err != nil {
+		return nil
+	}
+
+	valueStr, ok := valueRaw.(string)
+	if !ok {
+		return nil
+	}
+
+	timestamp, err := exif.ParseExifFullTimestamp(valueStr)
+	if err != nil {
+		return nil
+	}
+
+	return &timestamp
 }
