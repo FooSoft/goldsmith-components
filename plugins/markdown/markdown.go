e85f9fe42b783881941a4e917e5c6392e61684dd
Kevin Dangoor <kevindangoor@khanacademy.org>
2020-03-28 11:53:33 -0400

    Allows the substitution of a custom Goldmark parser.
    Goldmark is an extensible Markdown parser, and sometimes people
    might want to use their own additions or a different set of
    extensions. With this change, the user can provide their
    own goldmark.Markdown instance.

--- plugins/markdown/markdown.go
+++ plugins/markdown/markdown.go
@@ -19,11 +19,19 @@ import (
 
 // Markdown chainable context.
 type Markdown struct {
+	md goldmark.Markdown
 }
 
 // New creates a new instance of the Markdown plugin.
 func New() *Markdown {
 	return new(Markdown)
+}
+
+// WithGoldmark allows you to provide your own instance of Goldmark with
+// a custom selection of plugins. The default includes GFM and Typographer.
+func (plugin *Markdown) WithGoldmark(md goldmark.Markdown) *Markdown {
+	plugin.md = md
+	return plugin
 }
 
 func (*Markdown) Name() string {
@@ -35,6 +43,17 @@ func (plugin *Markdown) Initialize(context *goldsmith.Context) (goldsmith.Filter
 }
 
 func (plugin *Markdown) Process(context *goldsmith.Context, inputFile *goldsmith.File) error {
+	md := plugin.md
+	if md == nil {
+		plugin.md = goldmark.New(
+			goldmark.WithExtensions(extension.GFM, extension.Typographer),
+			goldmark.WithParserOptions(parser.WithAutoHeadingID()),
+			goldmark.WithRendererOptions(html.WithUnsafe()),
+		)
+		md = plugin.md
+	}
+
+
 	outputPath := strings.TrimSuffix(inputFile.Path(), path.Ext(inputFile.Path())) + ".html"
 	if outputFile := context.RetrieveCachedFile(outputPath, inputFile); outputFile != nil {
 		outputFile.Meta = inputFile.Meta
@@ -46,12 +65,6 @@ func (plugin *Markdown) Process(context *goldsmith.Context, inputFile *goldsmith
 	if _, err := dataIn.ReadFrom(inputFile); err != nil {
 		return err
 	}
-
-	md := goldmark.New(
-		goldmark.WithExtensions(extension.GFM, extension.Typographer),
-		goldmark.WithParserOptions(parser.WithAutoHeadingID()),
-		goldmark.WithRendererOptions(html.WithUnsafe()),
-	)
 
 	var dataOut bytes.Buffer
 	if err := md.Convert(dataIn.Bytes(), &dataOut); err != nil {
--- plugins/markdown/testdata/source/index.md
+++ plugins/markdown/testdata/source/index.md
@@ -2,6 +2,9 @@
 
 *"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."*
 
+Lorem
+:    Some generic text
+
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pharetra leo in tincidunt egestas. Morbi sed imperdiet
 eros, a fermentum magna. Donec commodo leo risus, eu fermentum libero tempor eget. Maecenas dolor est, accumsan quis
 ante non, efficitur facilisis enim. Sed eget tristique lacus, nec ultricies mi. Mauris ultricies eu est eget
