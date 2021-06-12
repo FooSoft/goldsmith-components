aadd49d5f239bc3ee12060c84a1057bfd17a4cf9
Alex Yatskov <alex@foosoft.net>
2021-06-12 10:51:08 -0700

    Update docs

--- plugins/forward/forward.go
+++ plugins/forward/forward.go
@@ -1,9 +1,11 @@
+// Package forward allows to create simple redirections for pages that have moved.
 package forward
 
 import (
 	"github.com/FooSoft/goldsmith"
 )
 
+// Forward plugin context.
 type Forward struct {
 	sourceMeta map[string]interface{}
 	pathMap    map[string]string
@@ -11,6 +13,7 @@ type Forward struct {
 	targetKey  string
 }
 
+// New creates a new instance of the Forward plugin.
 func New(sourceMeta map[string]interface{}) *Forward {
 	return &Forward{
 		sourceMeta: sourceMeta,
@@ -20,21 +23,25 @@ func New(sourceMeta map[string]interface{}) *Forward {
 	}
 }
 
+// AddPathMapping adds a single path mapping between an old path and a new path.
 func (plugin *Forward) AddPathMapping(sourcePath, targetPath string) *Forward {
 	plugin.pathMap[sourcePath] = targetPath
 	return plugin
 }
 
+// PathMap sets multiple path mappings between old paths and new paths.
 func (plugin *Forward) PathMap(pathMap map[string]string) *Forward {
 	plugin.pathMap = pathMap
 	return plugin
 }
 
+// SourceKey sets the metadata key used to access the old path (default: "PathOld").
 func (plugin *Forward) SourceKey(key string) *Forward {
 	plugin.sourceKey = key
 	return plugin
 }
 
+// SourceKey sets the metadata key used to access the new path (default: "PathNew").
 func (plugin *Forward) TargetKey(key string) *Forward {
 	plugin.targetKey = key
 	return plugin
